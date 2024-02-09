023951a69aea140fa7c03c41c9e5220f572c3dad
julian.castillo <julian.castillo@AULA14-PCPROFEE.JUANXXIII.net>
2024-02-09 21:15:20 +0100

    dsfasdf

---
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ *File:HelloWorld 
+ */
+package HelloWorld;
+
+/**
+ *Clase principal para HelloWorld 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
